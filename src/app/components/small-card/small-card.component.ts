93a123e693852a6ff559d1bba26509dfd871726a
Eric <ericarantes0@gmail.com>
2022-11-23 18:30:45 -0300

    commit final

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -11,6 +11,8 @@ export class SmallCardComponent implements OnInit {
   photoCover:string = ""
   @Input()
   cardDescription:string = ""
+  @Input()
+  Id:string = "0"
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "http://www.nasa.gov/sites/default/files/thumbnails/image/sls_rocket.jpg",
+    "title": "LANÇAMENTO DO PRIMEIRO FOGUETE ARTEMIS",
+    "description": "Primero lançamento é um teste de tecnologia, com futuros lançamentos tendo a intenção de colocar a primeira mulher na lua",
+  },
+  {
+    "id": "2",
+    "photo": "https://cdn.mos.cms.futurecdn.net/snbrHBRigvvzjxNGuUtcck.jpg",
+    "title": "FATOS INTERESSANTES SOBRE A LUA",
+    "description": "A Lua é o corpo celestial mais próximo à terra, a quase 230 milhões de kilometros de distância",
+  },
+  {
+    "id": "3",
+    "photo": "https://images.nature.com/original/magazine-assets/d41586-022-03819-w/d41586-022-03819-w_23737968.jpg",
+    "title": "PRIMEIRAS IMAGENS DO SATÉLITE ORION",
+    "description": "Satélite foi lançado no foguete Artemis-1 como teste do protótipo",
+  },
+  {
+    "id": "4",
+    "photo": "https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/nhq202211170007.jpg?itok=vfv0Dzjk",
+    "title": "JAPÃO CONTRIBUI PARA A EXPANSÃO DA BASE ESPACIAL",
+    "description": "Pais quer aumentar sua presença em órbita à terra, e aloca fundos para expandir base espacial",
+  },
+]
